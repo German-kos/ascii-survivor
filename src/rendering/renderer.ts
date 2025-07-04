c62852d5e88bfd2866c13058c9bc37b492ab801c
German <kostiakovg@gmail.com>
2025-07-05 00:14:45 +0300

    Separated render related functions to a render handling file

---
+++ src/rendering/renderer.ts
@@ -0,0 +1,17 @@
+import { CELL_WIDTH, CELL_HEIGHT } from "../constants/world-constants";
+
+export function renderChunk(ctx: CanvasRenderingContext2D, chunk: string[][]) {
+  chunk.forEach((row, y) => {
+    row.forEach((cell, x) => {
+      // Draw each cell in the grid
+      ctx.fillText(cell, x * CELL_WIDTH, y * CELL_HEIGHT + 50);
+    });
+  });
+}
+
+export function clearCanvas(
+  ctx: CanvasRenderingContext2D,
+  canvas: HTMLCanvasElement
+) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
