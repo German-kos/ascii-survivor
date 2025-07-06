9cca9f5e464de0e9f27ac7251ac5f2b59ab60f13
German <kostiakovg@gmail.com>
2025-07-06 20:35:56 +0300

    Added background rendering for game tiles.

--- src/rendering/renderer.ts
+++ src/rendering/renderer.ts
@@ -4,6 +4,7 @@ import {
   TILE_CHARS,
   TILE_COLORS,
   FONT,
+  TILE_BACKGROUNDS,
 } from "../constants/world-constants.js";
 import { Player } from "../game/player.js";
 
@@ -25,34 +26,41 @@ export function renderChunk(
       ctx.fillText(
         cell,
         x * CELL_WIDTH + CELL_WIDTH,
-        y * CELL_HEIGHT + CELL_HEIGHT * 1.9
+        y * CELL_HEIGHT + CELL_HEIGHT
       );
     });
   });
 }
 
-export function renderPlayer(
+export function renderBackground(
   ctx: CanvasRenderingContext2D,
-  playerGrid: string[][],
-  player: Player
+  chunk: string[][]
 ) {
-  console.log("I am here");
-  playerGrid.forEach((row, y) => {
+  chunk.forEach((row, y) => {
     row.forEach((cell, x) => {
-      if (x === player.x && y === player.y) {
-        cell = TILE_CHARS.PLAYER; // Use the player's character
-      }
       ctx.font = FONT;
       ctx.textAlign = "center";
       ctx.textBaseline = "middle";
-      ctx.fillStyle = getTileColor(cell);
-      ctx.fillText(
-        cell,
-        x * CELL_WIDTH + CELL_WIDTH,
-        y * CELL_HEIGHT + CELL_HEIGHT * 1.9
+      ctx.fillStyle = getTileBackground(cell);
+      ctx.fillRect(
+        x * CELL_WIDTH + CELL_WIDTH * 0.5,
+        y * CELL_HEIGHT + CELL_HEIGHT * 0.5,
+        CELL_WIDTH,
+        CELL_HEIGHT
       );
     });
   });
+}
+export function renderPlayer(ctx: CanvasRenderingContext2D, player: Player) {
+  ctx.font = FONT;
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillStyle = getTileColor(TILE_CHARS.PLAYER);
+  ctx.fillText(
+    TILE_CHARS.PLAYER,
+    player.x * CELL_WIDTH + CELL_WIDTH,
+    player.y * CELL_HEIGHT + CELL_HEIGHT
+  );
 }
 
 export function clearCanvas(
@@ -69,6 +77,13 @@ function getTileColor(tile: string): string {
   return tileKey ? TILE_COLORS[tileKey] : "white"; // Default to white if no color is found
 }
 
+function getTileBackground(tile: string): string {
+  const tileKey = Object.keys(TILE_CHARS).find(
+    (key) => TILE_CHARS[key as keyof typeof TILE_CHARS] === tile
+  ) as keyof typeof TILE_BACKGROUNDS | undefined;
+  return tileKey ? TILE_BACKGROUNDS[tileKey] : "white";
+}
+
 export function rerenderWorld(
   ctx: CanvasRenderingContext2D,
   chunk: string[][],
@@ -76,9 +91,10 @@ export function rerenderWorld(
 ) {
   clearCanvas(ctx, ctx.canvas);
   // TODO: Add render background here when implemented
+  renderBackground(ctx, chunk);
   renderChunk(ctx, chunk, {
     playerX: player.x,
     playerY: player.y,
   });
-  renderPlayer(ctx, chunk, player);
+  renderPlayer(ctx, player);
 }
