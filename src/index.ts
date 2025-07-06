647ddce3ab9039619c749a4040852164afc68816
German <kostiakovg@gmail.com>
2025-07-06 18:29:36 +0300

    Removed unused imports, removed unused and redundant code, extracted grid initializers into their own files.

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,47 @@
 import {
-  CELL_HEIGHT,
-  CELL_WIDTH,
+  CANVAS_SIZE,
   GRID_HEIGHT,
   GRID_WIDTH,
+  TILE_CHARS,
 } from "./constants/world-constants.js";
+import { initializePlayerGrid } from "./game/grid-initializers.js";
 import { Player } from "./game/player.js";
+import { renderChunk, renderPlayer } from "./rendering/renderer.js";
+// end of imports!
+
+// TODO: Try to integrate colors for different types of blocks.
 
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 const player = new Player();
 
-if (ctx) {
-  ctx.fillStyle = "white";
-}
+// Player grid for demo
+const playerGrid: string[][] = initializePlayerGrid(
+  new Array(GRID_HEIGHT).fill([])
+);
 
-// Chunk grid, initialized with empty arrays:
 const currentChunk: string[][] = [];
 for (let y = 0; y < GRID_HEIGHT; y++) {
   if (y === 5) {
-    currentChunk[y] = new Array(GRID_WIDTH).fill("O"); // Fill row 5 with "O" for testing "player.standingOn"
+    currentChunk[y] = new Array(GRID_WIDTH).fill(TILE_CHARS.GRASS);
   } else {
-    currentChunk[y] = new Array(GRID_WIDTH).fill("X");
+    currentChunk[y] = new Array(GRID_WIDTH).fill(TILE_CHARS.ROCK);
   }
 }
 
-// Make sure the canvas isn't null
 if (ctx) {
-  ctx.font = "16px monospace";
-  ctx.fillText("Welcome to ASCII Survivor!", 10, 30);
+  canvas.width = CANVAS_SIZE.WIDTH;
+  canvas.height = CANVAS_SIZE.HEIGHT;
 
-  currentChunk.forEach((row, y) => {
-    row.forEach((cell, x) => {
-      // Draw each cell in the grid
-      ctx.fillText(cell, x * CELL_WIDTH, y * CELL_HEIGHT + 50);
-    });
-  });
+  console.log("Finished setting up canvas");
+
+  renderChunk(ctx, currentChunk);
+  renderPlayer(ctx, playerGrid, player);
+
+  console.log("Finished drawing grid");
 
   addEventListener("keydown", (event) => {
     player.handleMove(event, currentChunk, ctx, canvas);
+    console.log("Player moved");
   });
 }
