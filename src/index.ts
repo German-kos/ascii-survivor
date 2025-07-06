e14670643dddf558db51e90aa29fa1367686a24a
German <kostiakovg@gmail.com>
2025-07-06 22:33:08 +0300

    Cleaned up the code further, added a configure canvas function.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,16 @@
-import { CANVAS_SIZE } from "./constants/world-constants.js";
 import { initializeDemoChunk } from "./game/grid-initializers.js";
 import { Player } from "./game/player.js";
-import {
-  renderBackground,
-  renderChunk,
-  renderPlayer,
-  rerenderWorld,
-} from "./rendering/renderer.js";
-// end of imports!
+import { configureCanvas } from "./rendering/canvas-utils.js";
+import { rerenderWorld } from "./rendering/renderer.js";
 
-// TODO: Try to integrate colors for different types of blocks.
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 const player = new Player();
 const currentChunk: string[][] = initializeDemoChunk();
 
 if (ctx) {
-  canvas.width = CANVAS_SIZE.WIDTH;
-  canvas.height = CANVAS_SIZE.HEIGHT;
-
-  console.log("Finished setting up canvas");
-
+  configureCanvas(canvas);
   rerenderWorld(ctx, currentChunk, player);
-
-  console.log("Finished drawing grid");
-
   addEventListener("keydown", (event) => {
     player.movePlayer(event, currentChunk, ctx, canvas);
     console.log("Player moved");
