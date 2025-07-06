b7768ddc0d3620c2b7d0190319932f5d5a495a69
German <kostiakovg@gmail.com>
2025-07-06 22:35:56 +0300

    Created a function to configure the canvas for startup.

---
+++ src/rendering/canvas-utils.ts
@@ -0,0 +1,11 @@
+import { CANVAS_SIZE } from "../constants/world-constants.js";
+
+export function configureCanvas(canvas: HTMLCanvasElement): void {
+  canvas.width = CANVAS_SIZE.WIDTH;
+  canvas.height = CANVAS_SIZE.HEIGHT;
+  const ctx = canvas.getContext("2d");
+  if (ctx) {
+    ctx.clearRect(0, 0, CANVAS_SIZE.WIDTH, CANVAS_SIZE.HEIGHT);
+    // ctx.imageSmoothingEnabled = true; // Remove if looks bad (supposed to smooth pixels);
+  }
+}
