67e33c2928eeb00643fb07245f46cacb112bf3cc
German <kostiakovg@gmail.com>
2025-07-07 22:43:03 +0300

    Removed old world generation function.

--- src/game/grid-initializers.ts
+++ src/game/grid-initializers.ts
@@ -1,35 +1,10 @@
-import {
-  GRID_HEIGHT,
-  GRID_WIDTH,
-  TILE_CHARS,
-} from "../constants/world-constants.js";
+import { GRID_HEIGHT, GRID_WIDTH } from "../constants/world-constants.js";
 import {
   createTile,
   TILE_CONFIG,
   TileConfig,
   TileType,
 } from "./tiles/index.js";
-
-export function initializeDemoChunk(): string[][] {
-  const chunk: string[][] = [];
-  for (let y = 0; y < GRID_HEIGHT; y++) {
-    chunk[y] = new Array(GRID_WIDTH).fill("●"); // Default to rock
-  }
-
-  // modify specific areas
-  for (let y = 0; y < GRID_HEIGHT; y++) {
-    for (let x = 0; x < GRID_WIDTH; x++) {
-      console.log("x:", x, "y:", y);
-      if (y === 5) {
-        chunk[y][x] = TILE_CHARS.GRASS;
-      } else if (x === 10) {
-        chunk[y][x] = TILE_CHARS.FLOWER;
-      }
-    }
-  }
-
-  return chunk;
-}
 
 export function generateDemoChunk(): TileConfig[][] {
   const chunk: TileConfig[][] = [];
@@ -46,10 +21,6 @@ export function generateDemoChunk(): TileConfig[][] {
     for (let x = 0; x < GRID_WIDTH; x++) {
       if (y <= 5 && x <= 5) {
         chunk[y][x] = createTile("grass");
-        // console.log("Random key:", key);
-        // console.log("TILE_CHARS[key]:", TILE_CHARS[key]);
-        // console.log("TILE_CHARS[key]:", TILE_CHARS[key]);
-        // chunk[y][x] = createTile(key) || TILE_CONFIG.EMPTY;
       } else {
         const key = keys[Math.floor(Math.random() * keys.length)] as TileType;
         chunk[y][x] = createTile(key) || TILE_CONFIG.EMPTY;
