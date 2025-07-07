104340aa53726d8cb226993a88ac651e8060e155
German <kostiakovg@gmail.com>
2025-07-07 20:30:20 +0300

    Added interface for the tile config, and necessary types.

--- src/game/tiles/tile-types.ts
+++ src/game/tiles/tile-types.ts
@@ -1,1 +1,38 @@
-// Declare configs for tile types for the factory
+export interface TileConfig {
+  name: string;
+  type: TileType;
+  sprite: TileSprite;
+  color: string;
+  background: string;
+  description: string;
+  canBeDestroyed: boolean;
+  canBeHarvested: boolean;
+  harvestAmount: number;
+  walkable: boolean;
+  health: number;
+  damage: number;
+  toolRequired: ToolType;
+  toolLevelRequired: ToolLevel;
+}
+
+type TileType =
+  | "empty"
+  | "wall"
+  | "tree"
+  | "water"
+  | "rock"
+  | "grass"
+  | "flower";
+
+type TileSprite = "." | "█" | "♠" | "≈" | "●" | "▓" | "*";
+``;
+type ToolLevel = "none" | "basic" | "advanced" | "expert" | "master";
+
+type ToolType =
+  | "none"
+  | "axe"
+  | "pickaxe"
+  | "shovel"
+  | "hoe"
+  | "sickle"
+  | "bucket";
