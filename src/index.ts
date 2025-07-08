7a5c9b75911c041dc9ddf9b233b7a7896c9ef9e4
German <kostiakovg@gmail.com>
2025-07-08 22:27:05 +0300

    Initializing the interactive cursor.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { generateDemoChunk } from "./game/grid-initializers.js";
-import { Player } from "./game/player.js";
+import { InteractiveCursor } from "./game/player/interactive-cursor.js";
+import { Player } from "./game/player/player.js";
 import { TileConfig } from "./game/tiles/tile-types.js";
 import { configureCanvas } from "./rendering/canvas-utils.js";
 import { renderWorld } from "./rendering/renderer.js";
@@ -7,12 +8,14 @@ import { renderWorld } from "./rendering/renderer.js";
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 const player = new Player();
+const cursor = new InteractiveCursor(player);
 const currentChunk: TileConfig[][] = generateDemoChunk();
 
 if (ctx) {
   configureCanvas(canvas);
-  renderWorld(ctx, currentChunk, player);
+  renderWorld(ctx, currentChunk, player, cursor);
   addEventListener("keydown", (event) => {
-    player.movePlayer(event, currentChunk, ctx, canvas);
+    player.movePlayer(event, currentChunk, ctx, cursor);
+    cursor.moveCursor(event, player, ctx, currentChunk);
   });
 }
