5574dcbbced89818d392bcc176be468bb7065634
German <kostiakovg@gmail.com>
2025-07-08 19:20:36 +0300

    Resized world, fixed render offset.

--- src/constants/world-constants.ts
+++ src/constants/world-constants.ts
@@ -1,13 +1,13 @@
 export const FONT = "bold 16px 'Courier New', monospace";
-export const GRID_WIDTH = 60;
+export const GRID_WIDTH = 40;
 export const GRID_HEIGHT = 25;
-export const CELL_WIDTH = 10;
+export const CELL_WIDTH = 16;
 export const CELL_HEIGHT = 16;
 export const CHUNK_WIDTH = GRID_WIDTH * CELL_WIDTH;
 export const CHUNK_HEIGHT = GRID_HEIGHT * CELL_HEIGHT;
 export const CANVAS_SIZE = {
-  WIDTH: CHUNK_WIDTH + CELL_WIDTH * 2,
-  HEIGHT: CHUNK_HEIGHT + CELL_HEIGHT * 2,
+  WIDTH: CHUNK_WIDTH,
+  HEIGHT: CHUNK_HEIGHT,
 };
 
 export const TILE_CHARS = {
