e3a26b1a984d3e4d3923952474720f457857b7ae
German <kostiakovg@gmail.com>
2025-07-10 12:39:06 +0300

    Cleaned up interaction handler, improved action flow.

--- src/game/player/interaction-handler.ts
+++ src/game/player/interaction-handler.ts
@@ -2,46 +2,88 @@ import { createTile, TileConfig } from "../tiles/index.js";
 import { InteractiveCursor } from "./interactive-cursor.js";
 import { Player } from "./player.js";
 
-// TODO: clean up this function with separate functions for better self documentations
-// TODO: Add something like "leavesBehind" to the Tile Config, so that it would be a bit
-// more automated for what happens when a tile is destroyed or harvested
 export function handleInteraction(
   currentChunk: TileConfig[][],
   player: Player,
-  cursor: InteractiveCursor,
-  ctx: CanvasRenderingContext2D
+  cursor: InteractiveCursor
 ) {
-  const highlightedTile = currentChunk[cursor.targetY][cursor.targetX];
-  if (!highlightedTile.canBeDestroyed && !highlightedTile.canBeHarvested) {
+  const tile = currentChunk[cursor.targetY][cursor.targetX];
+
+  if (!canTileBeHarvested(tile)) {
     console.log("Nothing I can do with this tile.");
     return;
   }
-  if (highlightedTile.toolRequired === "none") {
-    console.log("Harvesting " + highlightedTile.name + " without a tool.");
-    player.inventory.push(highlightedTile.type + " resource");
-    console.log(player.inventory);
-    currentChunk[cursor.targetY][cursor.targetX] = createTile("grass");
-  } else if (player.inventory.includes(highlightedTile.toolRequired)) {
-    console.log(
-      "Using " + highlightedTile.toolRequired + " on " + highlightedTile.name
-    );
-    if (highlightedTile.health > 0) {
-      highlightedTile.health -= 10;
-      console.log(
-        highlightedTile.name + " health is now " + highlightedTile.health
-      );
-      if (highlightedTile.health <= 0) {
-        console.log(highlightedTile.name + " has been destroyed.");
-        player.inventory.push(highlightedTile.type + " resource");
-        console.log(player.inventory);
-        currentChunk[cursor.targetY][cursor.targetX] = createTile("grass");
-      }
-    } else if (highlightedTile.health <= 0) {
-      console.log(highlightedTile.name + " has been destroyed.");
-      player.inventory.push(highlightedTile.type + " resource");
-      console.log(player.inventory);
-      currentChunk[cursor.targetY][cursor.targetX] = createTile("grass");
-    }
+
+  if (noToolRequired(tile)) {
+    harvestWithoutTool(tile, player, currentChunk, cursor);
+  } else if (hasRequiredTool(tile, player)) {
+    harvestWithTool(tile, player, currentChunk, cursor);
+  } else {
+    console.log(`I need a ${tile.toolRequired} to harvest this ${tile.name}.`);
   }
-  // player.inventory.push(highlightedTile.type + " resource");
+}
+
+function canTileBeHarvested(highlightedTile: TileConfig): boolean {
+  return highlightedTile.canBeHarvested;
+}
+
+function canTileBeDestroyed(highlightedTile: TileConfig): boolean {
+  return highlightedTile.canBeDestroyed;
+}
+
+function hasRequiredTool(highlightedTile: TileConfig, player: Player): boolean {
+  return player.inventory.includes(highlightedTile.toolRequired);
+}
+
+function noToolRequired(highlightedTile: TileConfig): boolean {
+  return highlightedTile.toolRequired === "none";
+}
+
+function addToInventory(player: Player, highlightedTile: TileConfig): void {
+  player.inventory.push(highlightedTile.type + " resource");
+  console.log(player.inventory);
+}
+
+function createTileFromConfig(
+  currentChunk: TileConfig[][],
+  cursor: InteractiveCursor,
+  destroyedTile: TileConfig
+): void {
+  currentChunk[cursor.targetY][cursor.targetX] = createTile(
+    destroyedTile.leavesBehind
+  );
+}
+
+function isTileDestroyed(highlightedTile: TileConfig): boolean {
+  return highlightedTile.health <= 0;
+}
+
+function harvestWithoutTool(
+  tile: TileConfig,
+  player: Player,
+  currentChunk: TileConfig[][],
+  cursor: InteractiveCursor
+) {
+  console.log(`Harvesting ${tile.name} without a tool.`);
+  addToInventory(player, tile);
+  if (canTileBeDestroyed(tile) && isTileDestroyed(tile)) {
+    createTileFromConfig(currentChunk, cursor, tile);
+  }
+}
+
+function harvestWithTool(
+  tile: TileConfig,
+  player: Player,
+  currentChunk: TileConfig[][],
+  cursor: InteractiveCursor
+) {
+  console.log(`Harvesting ${tile.name} with ${tile.toolRequired}`);
+  addToInventory(player, tile);
+  if (!isTileDestroyed(tile)) {
+    tile.health -= 10;
+    console.log(`${tile.name} HP: ${tile.health}`);
+  }
+  if (canTileBeDestroyed(tile) && isTileDestroyed(tile)) {
+    createTileFromConfig(currentChunk, cursor, tile);
+  }
 }
