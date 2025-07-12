556fbd2a20fcabddff992dd71b850deadb77298f
German <kostiakovg@gmail.com>
2025-07-12 22:26:32 +0300

    Index.ts now acts as an entry point, initializing only the inputHandler (input layer).

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import { InputHandler } from "./core/input-handler.js";
-import { generateDemoChunk } from "./game/grid-initializers.js";
-import { InteractiveCursor } from "./game/player/interactive-cursor.js";
-import { Player } from "./game/player/player.js";
-import { configureCanvas } from "./rendering/canvas-utils.js";
-import { renderWorld } from "./rendering/renderer.js";
-import { TileConfig } from "./types/interfaces.js";
+import { InputHandler } from "./core/index";
 
-const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
-const player = new Player();
-const cursor = new InteractiveCursor(player);
-const currentChunk: TileConfig[][] = generateDemoChunk();
-const inputHandler = new InputHandler(player, cursor, currentChunk, ctx!);
-
-if (ctx) {
-  configureCanvas(canvas);
-  renderWorld(ctx, currentChunk, player, cursor);
-}
+const inputHandler = new InputHandler();
