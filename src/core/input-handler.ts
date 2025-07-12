ffe5bf5553075591fa74d8263330ee17ff55cee1
German <kostiakovg@gmail.com>
2025-07-12 22:29:08 +0300

    Refactored the input handler to utilize the game controller and be the second layer of the code. (Now also calls an initial render for the canvas).

--- src/core/input-handler.ts
+++ src/core/input-handler.ts
@@ -1,28 +1,18 @@
-import { Game } from "../game/game.js";
-import { InteractiveCursor } from "../game/player/interactive-cursor.js";
-import { Player } from "../game/player/player.js";
-import { TileConfig } from "../types/interfaces.js";
+import { GameController } from "../game/game-controller/index.js";
+import { Direction } from "../types/index.js";
 
 export class InputHandler {
-  player: Player;
-  cursor: InteractiveCursor;
-  currentChunk: TileConfig[][];
-  ctx: CanvasRenderingContext2D;
-  game: Game;
+  game: GameController;
 
-  constructor(
-    player: Player,
-    cursor: InteractiveCursor,
-    currentChunk: TileConfig[][],
-    ctx: CanvasRenderingContext2D
-  ) {
-    this.player = player;
-    this.cursor = cursor;
-    this.currentChunk = currentChunk;
-    this.ctx = ctx;
-    this.game = new Game();
+  constructor() {
+    this.game = new GameController();
 
     this.initializeEventListeners();
+    this.initialRender();
+  }
+
+  private initialRender(): void {
+    this.game.executeInitialRender();
   }
 
   private initializeEventListeners(): void {
@@ -31,30 +21,48 @@ export class InputHandler {
     });
   }
 
-  private handleInput(event: KeyboardEvent, game: Game): void {
+  private handleInput(event: KeyboardEvent): void {
     switch (event.code) {
       case "KeyW":
       case "KeyA":
       case "KeyS":
       case "KeyD":
-        this.player.movePlayer(event, this.currentChunk, this.ctx, this.cursor);
+        this.game.movePlayer(this.getDirectionFromKey(event));
         break;
       case "ArrowUp":
       case "ArrowDown":
       case "ArrowLeft":
       case "ArrowRight":
+        this.game.moveCursor(this.getDirectionFromKey(event));
+        break;
       case "ControlLeft":
       case "ControlRight":
       case "Space":
-        this.cursor.handleInput(
-          event,
-          this.player,
-          this.ctx,
-          this.currentChunk
-        );
+        // TODO: implement interaction logic
+        // this.cursor.handleInput(
+        //   event,
+        //   this.player,
+        //   this.ctx,
+        //   this.currentChunk
+        // );
         break;
       default:
         break;
+    }
+  }
+
+  private getDirectionFromKey(event: KeyboardEvent): Direction {
+    switch (event.code) {
+      case "KeyW":
+        return "up";
+      case "KeyA":
+        return "left";
+      case "KeyS":
+        return "down";
+      case "KeyD":
+        return "right";
+      default:
+        throw new Error("Invalid direction key");
     }
   }
 }
