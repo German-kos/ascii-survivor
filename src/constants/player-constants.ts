a3a4a0f8e35435e7b44587f77eee14b822949eb4
German <kostiakovg@gmail.com>
2025-07-15 20:41:12 +0300

    Added player related constants.

---
+++ src/constants/player-constants.ts
@@ -0,0 +1,5 @@
+import { bronzeAxe, bronzePickaxe, bronzeRake } from "../game/index.js";
+import { Item } from "../types/index.js";
+
+export const DEFAULT_INVENTORY_SIZE: number = 32;
+export const DEFAULT_INVENTORY: Item[] = [bronzeAxe, bronzePickaxe, bronzeRake];
