c702eadb005cd36f2c13d058bcc77821c77bd769
German <kostiakovg@gmail.com>
2025-07-15 20:47:16 +0300

    Updated exports.

--- src/game/player/index.ts
+++ src/game/player/index.ts
@@ -1,5 +1,4 @@
 export * from "./cursor-system.js";
 export * from "./hotbar-system.js";
-export * from "./interaction-handler.js";
 export * from "./inventory-system.js";
 export * from "./player-system.js";
