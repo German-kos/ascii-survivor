e3e6c51b43a70f5cd53791330109e372ce920ac5
German <kostiakovg@gmail.com>
2025-07-15 20:44:38 +0300

    Renamed file to cursor-system.ts

---
+++ src/game/player/cursor-system.ts
@@ -0,0 +1,72 @@
+import { CursorColor, Direction } from "../../types/types.js";
+import { CursorRenderingParams, Position } from "../../types/index.js";
+import { CURSOR_COLORS } from "../../constants/index.js";
+
+export class CursorSystem {
+  private targetX: number;
+  private targetY: number;
+  private locked: boolean;
+  private color: CursorColor;
+
+  constructor(initialPosition: Position) {
+    this.targetX = initialPosition.x;
+    this.targetY = initialPosition.y;
+    this.locked = true;
+    this.color = this.locked
+      ? (CURSOR_COLORS.LOCKED as CursorColor)
+      : (CURSOR_COLORS.UNLOCKED as CursorColor);
+  }
+
+  getPosition(): Position {
+    return { x: this.targetX, y: this.targetY };
+  }
+
+  getRenderingParams(): CursorRenderingParams {
+    return {
+      position: this.getPosition(),
+      color: this.color,
+    };
+  }
+
+  getNextPositionLocked(
+    faceDirection: Direction,
+    playerPosition: Position
+  ): Position {
+    switch (faceDirection) {
+      case "up":
+        return { x: playerPosition.x, y: playerPosition.y - 1 };
+      case "down":
+        return { x: playerPosition.x, y: playerPosition.y + 1 };
+      case "left":
+        return { x: playerPosition.x - 1, y: playerPosition.y };
+      case "right":
+        return { x: playerPosition.x + 1, y: playerPosition.y };
+      default:
+        throw new Error("Unknown facing direction");
+    }
+  }
+
+  setPosition(position: Position): void {
+    if (position.x < 0 || position.y < 0) {
+      throw new Error("Position cannot be negative");
+    }
+    const { x, y } = position;
+    this.targetX = x;
+    this.targetY = y;
+  }
+
+  getCursorLockState(): boolean {
+    return this.locked;
+  }
+
+  toggleLockState(): void {
+    this.locked = !this.locked;
+    this.updateColor();
+  }
+
+  private updateColor() {
+    this.color = this.locked
+      ? (CURSOR_COLORS.LOCKED as CursorColor)
+      : (CURSOR_COLORS.UNLOCKED as CursorColor);
+  }
+}
