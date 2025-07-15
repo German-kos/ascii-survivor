eaf45584b377566c4b6cc92bd4988cf6deec50b4
German <kostiakovg@gmail.com>
2025-07-15 20:44:52 +0300

    Added a blueprint for future implementation of a hotbar.

---
+++ src/game/player/hotbar-system.ts
@@ -0,0 +1,24 @@
+// import { Item } from "../../types/interfaces";
+
+export class HotbarSystem {
+  constructor() {
+    // Initialize the hotbar system
+  }
+
+  // addItemToHotbar(item:Item) {
+  // Logic to add an item to the hotbar
+  // }
+
+  // removeItemFromHotbar(item: Item) {
+  // Logic to remove an item from the hotbar
+  // }
+
+  // useItemFromHotbar(slot) {
+  // Logic to use an item from a specific slot in the hotbar
+  // }
+
+  // getHotbarItems() {
+  // Logic to retrieve all items in the hotbar
+  // return [];
+  // }
+}
